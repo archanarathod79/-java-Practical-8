39dc550888f84c656072a352483f7484f42ba85d
archanarathod79 <archanarathod5043@gmail.com>
2025-02-17 00:33:21 +0530

    Add files via upload

---
+++ PracticalpD8.java
@@ -0,0 +1,8 @@
+public class PracticalpD8 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(" ".repeat(n - 1) + "*");
+        System.out.println(" ".repeat(n - 2) + "/".repeat(3));
+        System.out.println("*".repeat(5));
+    }
+}
